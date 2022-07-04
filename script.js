970ca150860af64947406851a6d137b14dd44b7a
Lucas Pedro <rodrigerioo@gmail.com>
2022-07-04 18:41:54 -0300

    finalmente 2 e 3 finalizados

--- script.js
+++
@@ -1,9 +0,0 @@
-const classeColor = document.getElementsByClassName(color)
-const cores = ['#000000', '#000080', '##00FF00', '#FF1493']
-
-function definirCores() {
-    for(let i = 0; i < classeColor.length; i += 1 ) {
-        classeColor[i].style.backgoundColor = cores[i]
-    }
-}
-definirCores()
