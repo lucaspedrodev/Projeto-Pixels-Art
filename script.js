e1088701bf30555086c3c5a5db4e638d2e071f82
Lucas Pedro <rodrigerioo@gmail.com>
2022-07-05 20:12:16 -0300

    requisito 07

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 const quadroDePixel = document.getElementById('pixel-board')
 const clic = document.getElementsByClassName('selected')
+const PaletaCores = document.getElementById('color-palette')
+const filhosCores = PaletaCores.children
+
+// 5
+
+function pixels(tamanho) {
 
 
-
-function pixels (tamanho){
-
-
-    for (let i = 0; i < tamanho * tamanho ; i += 1) {
+    for (let i = 0; i < tamanho * tamanho; i += 1) {
         const classPixel = document.createElement('classPixel')
         classPixel.classList.add('pixel')
+        classPixel.addEventListener('click', function () {
+            console.log('teste');
+        })
+
         quadroDePixel.appendChild(classPixel)
-
-
+        i
     }
 }
+
 pixels(5)
+
+// 7
+
+//for (let i = 0; i < filhosCores.length; i += 1) {
+//  filhosCores[i].addEventListener('click', function(e){
+//    console.log(filhosCores[i] );
+
+//if (filhosCores[i].classList.contains('selected')) {
+//  filhosCores[i].classList.remove('selected')    
+//}  e.target.classList.add('selected')
+//})
+
+//}
+
+PaletaCores.addEventListener('click', cores)
+
+function cores(event) {
+    for (let i = 0; i < filhosCores.length; i += 1) {
+        if (filhosCores[i].classList.contains('selected')) {
+            filhosCores[i].classList.remove('selected');
+        } event.target.classList.add('selected')
+    }
+}
+
 
